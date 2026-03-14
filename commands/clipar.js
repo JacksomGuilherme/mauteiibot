eb3f011a1c129c5d68c3a9bef0142ecce3e70ca6
JacksomGuilherme <jacksom.guilherme@hotmail.com>
2026-03-14 13:43:34 -0300

    feat: add new arg option to creat clipe with a specific duration between 5 and 60 seconds

--- commands/clipar.js
+++ commands/clipar.js
@@ -3,12 +3,38 @@ const { sendMessage } = require('../utils/messages')
 const { getValidAccessToken } = require('../authentication/twitchAuth.service')
 const { getBroadcasterId } = require('../utils/broadcast')
 
+function parseClipArgs(args) {
+    let seconds = null
+    let title = args.join(' ')
+
+    if (args.length > 0) {
+        const match = args[0].match(/^(\d+)s$/i)
+
+        if (match) {
+            seconds = parseInt(match[1])
+
+            // limite seguro
+            if (seconds < 5) seconds = 5
+            if (seconds > 60) seconds = 60
+
+            title = args.slice(1).join(' ')
+        }
+    }
+
+    return { seconds, title }
+}
+
 module.exports = {
     name: 'clip',
     aliases: ['clipar', 'clipe'],
     execute: async ({ client, channel, tags, args, fullArgs }) => {
         const accessToken = await getValidAccessToken()
         const broadcasterId = await getBroadcasterId(channel)
+
+        const { seconds, title } = parseClipArgs(args)
+
+        let duration = seconds != null ? seconds : 30
+
         try {
             let response = await axios.post(
                 `https://api.twitch.tv/helix/clips`,
@@ -19,7 +45,8 @@ module.exports = {
                         'Authorization': `Bearer ${accessToken}`
                     },
                     params: {
-                        title: fullArgs,
+                        title: title,
+                        duration: duration,
                         broadcaster_id: broadcasterId
                     }
                 }
@@ -28,7 +55,9 @@ module.exports = {
 
             let url = `https://clips.twitch.tv/${clip.id}`
 
-            sendMessage(channel, `Clipe "${fullArgs}" criado:\n\n${url}`)
+            let returnMessage = (title && title != '') ? `"${title}" ` : ""
+
+            sendMessage(channel, `Clipe ${returnMessage}criado:\n\n${url}`)
         } catch (error) {
             if (error.status === 404) {
                 sendMessage(channel, `Não é possível clipar um canal offline`)
